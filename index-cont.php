1f65957bbd227ab0b160e6e0d12b67ef952e6cd4
Fabian Bode <fabian@thesarus.local>
2014-04-18 19:15:03 +0200

    Initial

---
+++ index-cont.php
@@ -0,0 +1,41 @@
+<h1>Willkommen beim WM Tippspiel</h1>
+
+<p>
+
+	Die Endrunde der Fußball-Weltmeisterschaft 2014 (portugiesisch Copa do Mundo FIFA, englisch FIFA World Cup) der Männer ist die 20. Ausspielung des bedeutendsten Turniers für Fußball-Nationalmannschaften und findet vom 12. Juni bis zum 13. Juli 2014 in Brasilien statt. Titelverteidiger ist der Weltmeister von 2010, Spanien. Der Sieger ist automatisch für den FIFA-Konföderationen-Pokal 2017 qualifiziert.
+
+	Der Beschluss des FIFA-Exekutivkomitees, Brasilien zum zweiten Mal nach 1950 zum Gastgeber der Weltmeisterschaft zu ernennen, fiel am 30. Oktober 2007 in Zürich. Als Gastgeber der WM war Brasilien automatisch Ausrichter des Konföderationen-Pokals 2013.
+</p>
+<p>
+	Tabelle der aktuellen Teilnehmer
+</p>
+<table bgcolor="#FFFFFF" align="center">
+	<tr>
+		<td>Spieler</td>
+		<td>Punktestand</td>
+		
+		http://jasonweaver.name/lab/flexiblenavigation/
+	</tr>
+	<?php
+	// Datenbankverbindung herstellen
+	$sqlserver = "localhost";
+	$user = "root";
+	$pass = "";
+	$dbase = "tippspiel";
+	$conn = @mysql_connect($server, $user, $pass) or die("Fehler beim Datenbankzugriff");
+	mysql_select_db($dbase, $conn);
+	?>
+	<?php
+	$tabelle = mysql_query("SELECT * FROM user");
+	while ($z = mysql_fetch_array($tabelle)) {
+		echo '<tr>';
+		echo '<td>' . $z['username'] . '</td>' . '<td class="number">' . $z['point'] . '</td>';
+		echo "</tr>";
+	} //while
+	?>
+</table>
+
+<?php
+// Datenbank schließen
+mysql_close();
+?>
---
+++ login.php
@@ -0,0 +1,18 @@
+<h1>Willkommen beim WM Tippspiel</h1>
+
+<form>
+	<div id="left">
+		Nachname:<br>
+		Vorname:<br>
+		Benutzername:<br>
+		<input type="submit">
+	</div>
+	<div id="right">
+		<input type="text">
+		<br>
+		<input type="text">
+		<br>
+		<input type="text">
+		<br>
+	</div>
+</form>
