be9737a3f80984332e723efbe6e4c0d8307dbedf
marc7891 <marc_kaltenbach@web.de>
2014-05-12 18:31:08 +0200

    bla

--- index-cont.php
+++ index-cont.php
@@ -9,14 +9,15 @@ if (isset($_SESSION['username'])) {
 }; ?> beim WM Tippspiel</h1>
 
 <p>
-	egDie Endrunde der Fußball-Weltmeisterschaft 2014 (portugiesisch Copa do Mundo FIFA, englisch FIFA World Cup) der Männer ist die 20. Ausspielung des bedeutendsten Turniers für Fußball-Nationalmannschaften und findet vom 12. Juni bis zum 13. Juli 2014 in Brasilien statt. Titelverteidiger ist der Weltmeister von 2010, Spanien. Der Sieger ist automatisch für den FIFA-Konföderationen-Pokal 2017 qualifiziert.
-
-	Der Beschluss des FIFA-Exekutivkomitees, Brasilien zum zweiten Mal nach 1950 zum Gastgeber der Weltmeisterschaft zu ernennen, fiel am 30. Oktober 2007 in Zürich. Als Gastgeber der WM war Brasilien automatisch Ausrichter des Konföderationen-Pokals 2013.
+	TEXT MANU
+	
 </p>
 
 http://jasonweaver.name/lab/flexiblenavigation/
 
 http://docs.dev7studios.com/jquery-plugins/caroufredsel
+
+
 
 <p>
 	Tabelle der aktuellen Teilnehmer
--- nav.php
+++ nav.php
@@ -6,7 +6,7 @@ session_start();
 <script type="text/javascript">
 	jQuery(function() {
 		jQuery("#accordion").accordion({
-			heightStyle : "content",
+			highstyl: "content",
 			collapsible:true,
 			active:false
 		});
@@ -26,12 +26,31 @@ session_start();
 			</div>
 		</p>
 	</div>
-	<h3>Verzeichnis</h3>
+	<h3>Meine Tipps</h3>
 	<div>
 		<p>
 			<div id="ehemaligenverzeichnis" class="menuitem">
 				Ehemaligenverzeichnis
 			</div>
+		
+		</p>
+	</div>
+	<h3>Statistik</h3>
+	<div>
+		<p>
+			<div id="ehemaligenverzeichnis" class="menuitem">
+				......
+			</div>	
+			
+		</p>
+	</div>
+	<h3>Alles rund um die WM</h3>
+	<div>
+		<p>
+			<div id="ehemaligenverzeichnis" class="menuitem">
+				......
+			</div>	
+				
 			<?php
 if(isset($_SESSION["login"]) && $_SESSION["login"] == 1){
 			?>
